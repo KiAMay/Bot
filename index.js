d6fd4e446314425a081a38e81c7ecb1b3946408a
BuildTools <kieran_may@outlook.comMinecraft2005''git config --global user.email kieran_may@outlook.com>
2019-02-07 18:28:38 +0000

    BOT 18:23

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const ltocken = process.env.TOCKEN;
 require('dotenv/config');
 const http = require('http');
 const port = process.env.PORT || 3000;
-		  
+http.createServer().listen(port);	
+	  
 Client.on('message', (msg) =>{
 if (msg.content === '!>Help') {
   msg.channel.send('!>K');
